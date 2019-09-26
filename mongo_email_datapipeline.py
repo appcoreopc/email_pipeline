f8c7bd47ec90639d8997a2801d2447596d55f647
jeremy woo <kepungmoin@hotmail.com>
2019-09-26 21:13:28 +1200

    added jars support for mongodb

--- mongo_email_datapipeline.py
+++ mongo_email_datapipeline.py
@@ -1,14 +1,15 @@
+
+# Start the code with the followings..
+
+# pyspark --conf "spark.mongodb.input.uri=mongodb://127.0.0.1/apptest.product?readPreference=primaryPreferred" \
+#               --conf "spark.mongodb.output.uri=mongodb://127.0.0.1/apptest.product" \
+#               --packages org.mongodb.spark:mongo-spark-connector_2.11:2.4.1
+
+
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName("mysql_email_pipeline").getOrCreate()
+spark = SparkSession.builder.appName("mongo-email-pipeline").config("spark.mongodb.input.uri", "mongodb://127.0.0.1/apptest.product").config("spark.mongodb.output.uri", "mongodb://127.0.0.1/apptest.product").getOrCreate()
 
+df = spark.read.format("mongo").load()
 
-url = "jdbc:mongodb://localhost:27017/test"
-
-properties = {
-    "user": "foo",
-    "password": "bar"
-}
-
-df = spark.read.jdbc(url, table="SELET * FROM CUSTOMER", properties=properties)
-
+df.show()
