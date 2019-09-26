e62b44bd144373de00544bcb48410d1c8039af8c
jeremy woo <kepungmoin@hotmail.com>
2019-09-26 18:57:38 +1200

    addded spark stream processing

--- email_pipeline_stream.py
+++ email_pipeline_stream.py
@@ -5,26 +5,8 @@ from email.emailer import *
 
 spark = SparkSession.builder.appName("Email_pipeline_StreamFile").getOrCreate()
 
-
-# df = spark.read.format("csv").option("inferSchema", "false").option("header", "false").option("sep", ",").load("data/user.csv")
-
-# df = spark.readStream.option("inferSchema", "false").option("header", "false").option("sep", ",").csv("data/user.csv")
-
-# schema = StructType (
-#   Array(StructField("transactionId", StringType),
-#         StructField("customerId", StringType),
-#         StructField("itemId", StringType),
-#         StructField("amountPaid", StringType)))
-
-# df = spark.readStream.text('data/user.csv')
-
-
 userSchema = StructType().add("id", StringType(), True).add("name", StringType(), True).add("email", StringType(), True)
-df = spark.readStream.schema(userSchema).format("csv").load("data/user.csv") 
-df.writeStream.format('console').outputMode('append').start()
 
-# for eachrow in df.rdd.collect():
-#     mailer = Emailer()
-#     #mailer.send('test demo', eachrow[2])
+df = spark.readStream.option("header", "true").schema(userSchema).csv("data") 
 
-df.awaitTermination()
+df.writeStream.format('console').outputMode('append').start().awaitTermination()
