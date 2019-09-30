65dd4ed13b45851fa9d4af4060b13a9210eb72d2
jeremy woo <kepungmoin@hotmail.com>
2019-09-30 19:44:45 +1300

    added excel implementation

--- excel_email_datapipeline.py
+++ excel_email_datapipeline.py
@@ -1,7 +1,10 @@
 from com.crealytics.spark.excel import *
 
 ## using spark-submit with option to execute script from command line
-## spark-submit --packages com.crealytics:spark-excel_2.12:0.12.0 excel_email_datapipeline.py
+## spark-submit --packages spark-excel_2.11:0.11.1 excel_email_datapipeline.py
+
+## pyspark --packages spark-excel_2.11:0.11.1
+# pyspark --packages com.crealytics:spark-excel_2.11:0.11.1
 
 from pyspark.sql import SparkSession
 
@@ -14,8 +17,5 @@ spark = SparkSession.builder.appName("excel-email-pipeline").getOrCreate()
 
 
 df = spark.read.format("com.crealytics.spark.excel").option("useHeader", "true").option("inferSchema", "true").load("data/excel.xlsx")
-
-
-
 
 df.show()
