9cb47406a7ef94ca6f1cf325c45eca354c447d55
jeremy woo <kepungmoin@hotmail.com>
2019-09-30 19:33:39 +1300

    added excel reader

---
+++ excel_email_datapipeline.py
@@ -0,0 +1,21 @@
+from com.crealytics.spark.excel import *
+
+## using spark-submit with option to execute script from command line
+## spark-submit --packages com.crealytics:spark-excel_2.12:0.12.0 excel_email_datapipeline.py
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("excel-email-pipeline").getOrCreate()
+
+# df = spark.read.format("com.crealytics.spark.excel").load("data/excel.xlsx")
+
+
+# df = spark.read.format("com.crealytics.spark.excel").option("useHeader", "true").option("maxRowsInMemory", 20).option("excerptSize", 10).load("data/excel.xlsx")
+
+
+df = spark.read.format("com.crealytics.spark.excel").option("useHeader", "true").option("inferSchema", "true").load("data/excel.xlsx")
+
+
+
+
+df.show()
