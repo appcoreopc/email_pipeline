e4f317045c6a722b3bb4bbd8236a3879960d1fdb
jeremy woo <kepungmoin@hotmail.com>
2019-09-30 20:59:42 +1300

    updated excel

--- excel_email_datapipeline.py
+++ excel_email_datapipeline.py
@@ -4,17 +4,11 @@ from com.crealytics.spark.excel import *
 ## spark-submit --packages spark-excel_2.11:0.11.1 excel_email_datapipeline.py
 
 ## pyspark --packages spark-excel_2.11:0.11.1
-# pyspark --packages com.crealytics:spark-excel_2.11:0.11.1
+## pyspark --packages com.crealytics:spark-excel_2.11:0.11.1
 
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("excel-email-pipeline").getOrCreate()
-
-# df = spark.read.format("com.crealytics.spark.excel").load("data/excel.xlsx")
-
-
-# df = spark.read.format("com.crealytics.spark.excel").option("useHeader", "true").option("maxRowsInMemory", 20).option("excerptSize", 10).load("data/excel.xlsx")
-
 
 df = spark.read.format("com.crealytics.spark.excel").option("useHeader", "true").option("inferSchema", "true").load("data/excel.xlsx")
 
